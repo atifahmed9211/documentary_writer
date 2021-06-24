ce6225292bad7f68857d5507228cb1063f65cb8b
atifahmed9211 <atif.ahmed9211@gmail.com>
2021-06-24 14:16:18 +0500

    24/6/2021_finalize

--- scriptingApp/static/js/atif.js
+++ scriptingApp/static/js/atif.js
@@ -5,147 +5,88 @@ function addRowTable(obj)
   {
     obj.rowCount++;
     //by default row
-    var add_newrow="<tr id='custom_script_row'><td class='newTD_custom_script' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD' class='lastTD'></td></tr>";
+    var add_newrow="<tr id='custom_script_row_"+obj.rowCount+"'><td class='newTD_custom_script' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD' class='lastTD'></td></tr>";
     $("#"+(obj.focussed_table)).append(add_newrow);
     console.log(obj.focussed_table);
-    $(".newTD").focus();
+    $(".newTD_custom_script").focus();
     obj.tabClick=0;
     //if user changes row type
     script_value=$("#script_type_value option:selected").val();
     console.log(script_value);
-    first_phase_VO=true;
-    first_phase_SOT=true;
-    first_phase_BLACK=true;
-    first_phase_OPENING_TITLES=true;
-    first_phase_CREDITS=true;
     $("#script_type_value").on('change',function(){
       script_value=$("#script_type_value option:selected").val();
       console.log(script_value);
       if(script_value=="VO")
       {
-        if(first_phase_VO)
-        {
-          $("#custom_script_row").hide();
-          var add_newrow="<tr id='VO_script_row'><td class='newTD_vo' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'  class='lastTD'>VO</td></tr>";
-          $("#script-table").append(add_newrow);
+          $("#custom_script_row_"+obj.rowCount).remove();
+          $("#SOT_script_row_"+obj.rowCount).remove();
+          $("#BLACK_script_row_"+obj.rowCount).remove();
+          $("#TITLES_script_row_"+obj.rowCount).remove();
+          $("#CREDITS_script_row_"+obj.rowCount).remove();
+          var add_newrow="<tr id='VO_script_row_"+obj.rowCount+"'><td class='newTD_vo' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'  class='lastTD'>VO</td></tr>";
+          $("#"+(obj.focussed_table)).append(add_newrow);
           $(".newTD_vo").focus();
           obj.tabClick=0;
-          first_phase_VO=false;
-        }
-        else
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").show();
-          $("#SOT_script_row").hide();
-          $("#BLACK_script_row").hide();
-          $("#TITLES_script_row").hide();
-          $("#CREDITS_script_row").hide();
-        }
       }
       else if(script_value=="SOT")
       {
-        if(first_phase_SOT)
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          var add_newrow="<tr id='SOT_script_row'><td class='newTD_sot' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>SOT</td></tr>";
-          $("#script-table").append(add_newrow);
+          $("#custom_script_row_"+obj.rowCount).remove();
+          $("#VO_script_row_"+obj.rowCount).remove();
+          $("#BLACK_script_row_"+obj.rowCount).remove();
+          $("#TITLES_script_row_"+obj.rowCount).remove();
+          $("#CREDITS_script_row_"+obj.rowCount).remove();
+          var add_newrow="<tr id='SOT_script_row_"+obj.rowCount+"'><td class='newTD_sot' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>SOT</td></tr>";
+          $("#"+(obj.focussed_table)).append(add_newrow);
           $(".newTD_sot").focus();
           obj.tabClick=0;
-          first_phase_SOT=false;
-        }
-        else
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").show();
-          $("#BLACK_script_row").hide();
-          $("#TITLES_script_row").hide();
-          $("#CREDITS_script_row").hide();
-        }
       }
       else if(script_value=="BLACK")
       {
-        if(first_phase_BLACK)
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").hide();
-          var add_newrow="<tr id='BLACK_script_row'><td class='newTD_black' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>BLACK</td></tr>";
-          $("#script-table").append(add_newrow);
+          $("#custom_script_row_"+obj.rowCount).remove();
+          $("#VO_script_row_"+obj.rowCount).remove();
+          $("#SOT_script_row_"+obj.rowCount).remove();
+          $("#TITLES_script_row_"+obj.rowCount).remove();
+          $("#CREDITS_script_row_"+obj.rowCount).remove();
+          var add_newrow="<tr id='BLACK_script_row_"+obj.rowCount+"'><td class='newTD_black' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>BLACK</td></tr>";
+          $("#"+(obj.focussed_table)).append(add_newrow);
           $(".newTD_black").focus();
           obj.tabClick=0;
-          first_phase_BLACK=false;
-        }
-        else
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").hide();
-          $("#BLACK_script_row").show();
-          $("#TITLES_script_row").hide();
-          $("#CREDITS_script_row").hide();
-        }
       }
       else if(script_value=="OPENING TITLES")
       {
-        if(first_phase_OPENING_TITLES)
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").hide();
-          $("#BLACK_script_row").hide();
-          var add_newrow="<tr id='TITLES_script_row'><td class='newTD_titles' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>OPENING TITLES</td></tr>";
-          $("#script-table").append(add_newrow);
+          $("#custom_script_row_"+obj.rowCount).remove();
+          $("#VO_script_row_"+obj.rowCount).remove();
+          $("#SOT_script_row_"+obj.rowCount).remove();
+          $("#BLACK_script_row_"+obj.rowCount).remove();
+          $("#CREDITS_script_row_"+obj.rowCount).remove();
+          var add_newrow="<tr id='TITLES_script_row_"+obj.rowCount+"'><td class='newTD_titles' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>OPENING TITLES</td></tr>";
+          $("#"+(obj.focussed_table)).append(add_newrow);
           $(".newTD_titles").focus();
           obj.tabClick=0;
-          first_phase_OPENING_TITLES=false;
-        }
-        else
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").hide();
-          $("#BLACK_script_row").hide();
-          $("#TITLES_script_row").show();
-          $("#CREDITS_script_row").hide();
-        }
       }
       else if(script_value=="CREDITS")
       {
-        if(first_phase_CREDITS)
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").hide();
-          $("#BLACK_script_row").hide();
-          $("#TITLES_script_row").hide();
-          var add_newrow="<tr id='CREDITS_script_row'><td class='newTD_credits' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>CREDITS</td></tr>";
-          $("#script-table").append(add_newrow);
+          $("#custom_script_row_"+obj.rowCount).remove();
+          $("#VO_script_row_"+obj.rowCount).remove();
+          $("#SOT_script_row_"+obj.rowCount).remove();
+          $("#BLACK_script_row_"+obj.rowCount).remove();
+          $("#TITLES_script_row_"+obj.rowCount).remove();
+          var add_newrow="<tr id='CREDITS_script_row_"+obj.rowCount+"'><td class='newTD_credits' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD'>CREDITS</td></tr>";
+          $("#"+(obj.focussed_table)).append(add_newrow);
           $(".newTD_credits").focus();
           obj.tabClick=0;
-          first_phase_CREDITS=false;
-        }
-        else
-        {
-          $("#custom_script_row").hide();
-          $("#VO_script_row").hide();
-          $("#SOT_script_row").hide();
-          $("#BLACK_script_row").hide();
-          $("#TITLES_script_row").hide();
-          $("#CREDITS_script_row").show();
-        }
       }
       else
       {
-        $("#VO_script_row").hide();
-        $("#SOT_script_row").hide();
-        $("#BLACK_script_row").hide();
-        $("#TITLES_script_row").hide();
-        $("#CREDITS_script_row").hide();
-        $("#custom_script_row").show();
-        $(".newTD").blur();
-        $(".newTD_custom_script").focus();
+        $("#VO_script_row_"+obj.rowCount).remove();
+        $("#SOT_script_row_"+obj.rowCount).remove();
+        $("#BLACK_script_row_"+obj.rowCount).remove();
+        $("#TITLES_script_row_"+obj.rowCount).remove();
+        $("#CREDITS_script_row_"+obj.rowCount).remove();
+        var add_newrow="<tr id='custom_script_row_"+obj.rowCount+"'><td class='newTD_custom_script' contenteditable>"+obj.rowCount+"</td><td contenteditable></td><td contenteditable id='table_"+obj.tableCount+"lastTD' class='lastTD'></td></tr>";
+        $("#"+(obj.focussed_table)).append(add_newrow);
+        console.log(obj.focussed_table);
+        $(".newTD").focus();
         obj.tabClick=0;
       }
     });
